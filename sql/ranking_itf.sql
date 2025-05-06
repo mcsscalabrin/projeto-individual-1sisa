fa6aa2d2fa7f89d5521f68045e7434f5849236e2
mcsscalabrin <matheus.scalabrin@sptech.school>
2025-05-06 12:50:25 -0300

    atualizando nav bar e organização do script do bd dos rankings masc e fem

--- sql/ranking_itf.sql
+++ sql/ranking_itf.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE ranking_itf;
 USE ranking_itf;
 
+-- RANKING MASCULINO
 CREATE TABLE ranking_masc (
 	idAtleta int primary key auto_increment,
 	nome varchar(100),
@@ -14,6 +15,7 @@ CREATE TABLE ranking_masc (
 		references ranking_masc(idAtleta)
 );
 
+-- INSERINDO O TOP 10 MASCULINO DO RANKING ITF DE BEACH TENNIS
 INSERT INTO ranking_masc (nome, anoNasc, paisDeOrigem, torneiosJogados, pontos, ranking_itf) VALUES
 	('André Baran', 1991, 'Brasil', 22, 5800, 1),
     ('Michele Cappelletti', 1992, 'Itália', 23, 5800, 1),
@@ -25,31 +27,6 @@ INSERT INTO ranking_masc (nome, anoNasc, paisDeOrigem, torneiosJogados, pontos, 
     ('Antonio Miguel Ramos Vieira', 1993, 'Espanha', 30, 1970, 8),
     ('Leonardo Garrossino Branco', 2001, 'Brasil', 32, 1920, 9),
     ('Tommaso Giovannini', 1995, 'Itália', 13, 1881, 10);
-
-CREATE TABLE ranking_fem (
-	idAtleta int primary key auto_increment,
-	nome varchar(100),
-	anoNasc smallint,
-	paisDeOrigem varchar(45),
-	torneiosJogados int,
-	pontos int,
-	ranking_itf int,
-	fkDupla int,
-	constraint fkJogadoraDupla foreign key (fkDupla)
-		references ranking_fem(idAtleta)
-);
-
-INSERT INTO ranking_fem (nome, anoNasc, paisDeOrigem, torneiosJogados, pontos, ranking_itf) VALUES
-	('Giulia Gasparri', 1991, 'Itália', 22, 5492, 1),
-	('Ninny Valentini', 1999, 'Itália', 22, 5492, 1),
-	('Patricia Diaz', 1991, 'Venezuela', 19, 4248, 3),
-	('Rafaella Miiller', 1993, 'Brasil', 19, 4248, 3),
-	('Sophia Chow', 1997, 'Brasil', 21, 3940, 5),
-	('Vitoria Marchezini', 2005, 'Brasil', 24, 3940, 5),
-	('Flaminia Daina', 1993, 'Itália', 28, 2856, 7),
-	('Nicole Nobile', 1995, 'Itália', 26, 2743, 8),
-	('Veronica Casadei', 1998, 'Itália', 24, 2651, 9),
-	('Elizaveta Kudinova', 2004, 'Rússia', 21, 2568, 10);
 
 SELECT * FROM ranking_masc;
 
@@ -66,6 +43,34 @@ SELECT ranking.nome, dupla.nome as 'Dupla Fixa'
 	FROM ranking_masc as ranking
 	JOIN ranking_masc as dupla
     ON ranking.fkDupla = dupla.idAtleta;
+
+
+-- RANKING FEMININO
+CREATE TABLE ranking_fem (
+	idAtleta int primary key auto_increment,
+	nome varchar(100),
+	anoNasc smallint,
+	paisDeOrigem varchar(45),
+	torneiosJogados int,
+	pontos int,
+	ranking_itf int,
+	fkDupla int,
+	constraint fkJogadoraDupla foreign key (fkDupla)
+		references ranking_fem(idAtleta)
+);
+
+-- INSERINDO O TOP 10 FEMININO DO RANKING ITF DE BEACH TENNIS
+INSERT INTO ranking_fem (nome, anoNasc, paisDeOrigem, torneiosJogados, pontos, ranking_itf) VALUES
+	('Giulia Gasparri', 1991, 'Itália', 22, 5492, 1),
+	('Ninny Valentini', 1999, 'Itália', 22, 5492, 1),
+	('Patricia Diaz', 1991, 'Venezuela', 19, 4248, 3),
+	('Rafaella Miiller', 1993, 'Brasil', 19, 4248, 3),
+	('Sophia Chow', 1997, 'Brasil', 21, 3940, 5),
+	('Vitoria Marchezini', 2005, 'Brasil', 24, 3940, 5),
+	('Flaminia Daina', 1993, 'Itália', 28, 2856, 7),
+	('Nicole Nobile', 1995, 'Itália', 26, 2743, 8),
+	('Veronica Casadei', 1998, 'Itália', 24, 2651, 9),
+	('Elizaveta Kudinova', 2004, 'Rússia', 21, 2568, 10);
 
 SELECT * FROM ranking_fem;
 

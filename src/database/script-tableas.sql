514ee7f316606759deb9e70e294a638a6bae3557
mcsscalabrin <matheus.scalabrin@sptech.school>
2025-05-13 18:32:45 -0300

    adicionando arquivo da api (ainda nao está adaptada com a minha regra de negócio)

--- src/database/script-tableas.sql
+++ src/database/script-tableas.sql
@@ -1,32 +1,29 @@
 -- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
 -- Você precisa executar os comandos no banco de dados para criar as tabelas,
 -- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
-/*
- comandos para mysql server
- */
 CREATE DATABASE db_beachtennis;
 USE db_beachtennis;
-/*
- CREATE TABLE empresa (
- id INT PRIMARY KEY AUTO_INCREMENT,
- razao_social VARCHAR(50),
- cnpj CHAR(14),
- codigo_ativacao VARCHAR(50)
- );
- */
 CREATE TABLE usuario (
-    idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(50),
     email VARCHAR(50),
     senha VARCHAR(50),
+    fkAtletaFavorito INT,
+    FOREIGN KEY (fkAtletaFavorito) REFERENCES beach_tenista(idAtleta),
     categoria VARCHAR(50) constraint chkCategoria check (categoria in ('PRO', 'D', 'C', 'B', 'A'))
 );
-create table registro_quiz (
+CREATE TABLE registro_quiz (
     idTentativa INT PRIMARY KEY AUTO_INCREMENT,
-    dtHora DATETIME DEFAULT CURRENTTIMESTAMP(),
+    dtHora DATETIME DEFAULT current_timestamp,
     qtdAcertos INT,
     qtdErros INT,
     qtdAcertos INT,
     fkUsuario INT,
     FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario)
+);
+CREATE TABLE beach_tenista (
+    idAtleta INT PRIMARY KEY AUTO_INCREMENT,
+    genero VARCHAR(45),
+    pontuacao VARCHAR(45),
+    posicao VARCHAR(45)
 );
